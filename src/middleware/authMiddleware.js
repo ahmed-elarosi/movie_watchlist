b22663ce7c4a14a364f805e34000bf3fcabc44f4
Ahmed Elarosi <cabry95@protonmail.com>
2026-03-30 07:32:45 +0200

    add authMiddleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,2 +1,36 @@
+import "dotenv/config";
+
 import jwt from "jsonwebtoken";
 import { prisma } from "../config/db.js";
+
+// Read The Token From Request
+// Check If Token is valid
+export const authMiddleware = async (req, res, next) => {
+    console.log("Auth Middleware Reached");
+
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies?.jwt) {
+        token = req.cookies.jwt;
+    }
+    if (!token) {
+        return res.status(401).json({ error: "Not authorized, no token provided" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await prisma.user.findUnique({
+            where: { id: decoded.id },
+        });
+
+        if (!user) {
+            return res.status(401).json({ error: "User no longer exists!" });
+        }
+        req.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: "Not Authorized" });
+    }
+};
