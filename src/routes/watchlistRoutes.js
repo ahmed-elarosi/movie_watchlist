54554d2f467de491529544b87625ddffccd5b809
Ahmed Elarosi <cabry95@protonmail.com>
2026-03-31 11:22:53 +0200

    update validator

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -5,11 +5,13 @@ import {
     updateWatchlistItem,
 } from "../controller/watchlistController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { addToWatchlistSchema } from "../validators/watchlistValidators.js";
 
 const router = express.Router();
 router.use(authMiddleware);
 
-router.post("/", addToWatchlist);
+router.post("/", validateRequest(addToWatchlistSchema), addToWatchlist);
 
 router.put("/:id", updateWatchlistItem);
 
